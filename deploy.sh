ddee5a1a88ff2899c2380423e70b70b51ab59c4a
Vadim Gore <vadimgore@hotmail.com>
2018-11-30 13:38:37 -0800

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t vadimgore/multi-client:latest -t vadimgore/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t vadimgore/multi-server:latest -t vadimgore/multi-server:$SHA ./server/Dockerfile ./server
+docker build -t vadimgore/multi-worker:latest -t vadimgore/multi-worker:$SHA ./worker/Dockerfile ./worker
+docker push vadimgore/multi-client:latest
+docker push vadimgore/multi-server:latest
+docker push vadimgore/multi-worker:latest
+docker push vadimgore/multi-client:$SHA
+docker push vadimgore/multi-server:$SHA
+docker push vadimgore/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=vadimgore/multi-server:$SHA
+kubectl set image deployments/client-deployment client=vadimgore/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=vadimgore/multi-worker:$SHA
